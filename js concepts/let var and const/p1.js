159440bafeb59432877b45175fe3138d0ce6cc21
vermapushan25 <pushanverma.99@gmail.com>
2022-12-29 17:30:49 +0530

    adding concepts

---
+++ js concepts/let var and const/p1.js
@@ -0,0 +1,25 @@
+// 📌 Accessing variables before declaring
+
+console.log(a,"....a");
+console.log(b,"....b");  
+console.log(c,"....c");
+
+var a=12;
+let b="hello"
+const c=13;
+
+// 📌 Accessing variables after declaring
+console.log(a,"....a");
+console.log(b,"....b");
+console.log(c,"....c");
+
+
+
+
+
+
+
+
+
+
+
---
+++ js concepts/let var and const/p5.js
@@ -0,0 +1,37 @@
+// 📌 Updating variables 
+
+
+// 🔔 VAR
+// a="verma"
+// console.log(a,"...changing value even before declaring ")
+// var a="hello";
+// a="pushan";
+// console.log(a,"....changing value after declaring")
+
+
+
+
+// 🔔 LET
+// let b="pushan"
+// console.log(b,"......printing");
+// b="pushanverma";
+// console.log(b,"......updating 1st time");
+
+
+
+// 🔔 CONST
+
+const c="pushan"
+console.log(c,"....printing");
+c="pushanverma";
+console.log(c,"....updating 1st time");
+
+//  🌙 const cannot be upated, because as the definition of the word tells , it will remain constant
+// it will not change , throughout the program .
+
+
+
+
+// 👉 var can be updated
+
+
